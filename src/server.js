914ff9703ed5e94af121283c87d16df9823bfcc1
Marcus Reaiche <marcusreaiche@gmail.com>
2020-01-28 21:16:43 -0300

    Add sucrase as a devDependency and configure nodemon

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const server = require("./app");
+import "dotenv/config";
+import app from "./app";
 
-server.listen(process.env.HOST_PORT, () => {
+app.listen(process.env.HOST_PORT, () => {
   console.log(`Server is listening on http://localhost:${process.env.HOST_PORT}`);
 });
