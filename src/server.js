270f684c49e321f0d13d236d2f87957b3132bc3b
Marcus Reaiche <marcusreaiche@gmail.com>
2020-01-28 21:52:02 -0300

    Add and configure eslint and prettier

--- src/server.js
+++ src/server.js
@@ -2,5 +2,7 @@ import "dotenv/config";
 import app from "./app";
 
 app.listen(process.env.HOST_PORT, () => {
-  console.log(`Server is listening on http://localhost:${process.env.HOST_PORT}`);
+  console.log(
+    `Server is listening on http://localhost:${process.env.HOST_PORT}`
+  );
 });
